7f36e1f51e3fc8948344f836b5f41861ebddf250
ryanascherr <ryanascherr@gmail.com>
2021-07-15 09:52:51 -0400

    Added information and links on hover for tech blog.

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -13,7 +13,15 @@ export default function Portfolio() {
               <div className="pic-container">
                   <img src={techPic} alt=""></img>
                   <div className="overlay">
-                      <div className="text"><h1>Hello!</h1></div>
+                      <div className="text">
+                          <h2>Tech Blog</h2>
+                          <h3>Technologies Used:</h3>
+                          <p>HTML, CSS, JS, JQuery, Express, MySQL, Sequelize, Handlebars.js, Dotenv, Bcrypt</p>
+                          <div className="buttons">
+                              <a href="https://github.com/ryanascherr/tech-blog" target="_blank"><button className="button">Repo</button></a>
+                              <a href="https://whispering-sea-48503.herokuapp.com/" target="_blank"><button className="button">Web Page</button></a>
+                          </div>
+                      </div>
                   </div>
               </div>
           </div>

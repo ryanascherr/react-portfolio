dc1c748cd584fb9f6e66208fce233e4699c596cd
ryanascherr <ryanascherr@gmail.com>
2021-07-15 09:39:52 -0400

    Added more stylings to portfolio page. Added images of projects to portfolio page. Added hover effect to images on portfolio page.

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,13 +1,45 @@
 import React from 'react';
-import logo from '../../img/logo192.png'
+import techPic from '../../img/tech-blog.png';
+import yodaPic from '../../img/yoda.png';
+import weatherPic from '../../img/weather.png';
+import horiseonPic from '../../img/horiseon.png';
 
 export default function Portfolio() {
   return (
-    <div>
+    <div className="portfolio">
       <h1>My Portfolio</h1>
       <div className="projects">
           <div className="project">
-              <img src={logo} alt=""></img>
+              <div className="pic-container">
+                  <img src={techPic} alt=""></img>
+                  <div className="overlay">
+                      <div className="text"><h1>Hello!</h1></div>
+                  </div>
+              </div>
+          </div>
+          <div className="project">
+              <div className="pic-container">
+                  <img src={yodaPic} alt=""></img>
+                  <div className="overlay">
+                      <div className="text"><h1>Hello!</h1></div>
+                  </div>
+              </div>
+          </div>
+          <div className="project">
+              <div className="pic-container">
+                  <img src={weatherPic} alt=""></img>
+                  <div className="overlay">
+                      <div className="text"><h1>Hello!</h1></div>
+                  </div>
+              </div>
+          </div>
+          <div className="project">
+              <div className="pic-container">
+                  <img src={horiseonPic} alt=""></img>
+                  <div className="overlay">
+                      <div className="text"><h1>Hello!</h1></div>
+                  </div>
+              </div>
           </div>
       </div>
     </div>

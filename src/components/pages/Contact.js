93180a137e87a34aaa045f80638d07c072e89f10
ryanascherr <ryanascherr@gmail.com>
2021-07-15 20:11:26 -0400

    Further styled contact page.

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -5,9 +5,9 @@ export default function Contact() {
     <div className="contact">
       <h1>Get in Touch</h1>
       <div className="contact-links">
-        <a href="mailto: ryanascherr@gmail.com" target="_blank"><button className="link">ryanascherr@gmail.com</button></a>
-        <a href="https://github.com/ryanascherr" target="_blank"><button className="link">github.com/ryanascherr</button></a>
-        <a href="https://www.linkedin.com/in/ryanascherr/" target="_blank"><button className="link">linkedin.com/in/ryanascherr</button></a>
+        <a href="mailto: ryanascherr@gmail.com" target="_blank"><button className="link"><p>Email</p><p>ryanascherr@gmail.com</p></button></a>
+        <a href="https://github.com/ryanascherr" target="_blank"><button className="link"><p>GitHub</p><p>github.com/ryanascherr</p></button></a>
+        <a href="https://www.linkedin.com/in/ryanascherr/" target="_blank"><button className="link"><p>LinkedIn</p><p>linkedin.com/in/ryanascherr</p></button></a>
       </div>
     </div>
   );

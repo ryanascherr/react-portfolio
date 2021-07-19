78d16a642dd8d7d84bee525b6ed0dacf13cd025d
ryanascherr <ryanascherr@gmail.com>
2021-07-18 22:08:13 -0400

    Restructured contact page.

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,14 +1,31 @@
 import React from 'react';
 
 export default function Contact() {
+
+  function sendMail() {
+    
+    
+    var link = "mailto:ryanascherr@gmail.com"
+             + "?cc=myCCaddress@example.com"
+             + "&subject=" + encodeURIComponent("Hey Ryan, you're super cool!")
+             + "&body=" + encodeURIComponent(document.getElementById('email-body').value)
+    ;
+    
+    window.location.href = link;
+}
+
   return (
     <div className="contact">
       <h1>Get in Touch</h1>
-      <div className="contact-links">
-        <a href="mailto: ryanascherr@gmail.com" target="_blank"><button className="link"><p>Email</p><p>ryanascherr@gmail.com</p></button></a>
-        <a href="https://github.com/ryanascherr" target="_blank"><button className="link"><p>GitHub</p><p>github.com/ryanascherr</p></button></a>
-        <a href="https://www.linkedin.com/in/ryanascherr/" target="_blank"><button className="link"><p>LinkedIn</p><p>linkedin.com/in/ryanascherr</p></button></a>
-      </div>
+      <form>
+        <h3>Name:</h3>
+        <input type="text" id="fname" name="fname"></input><br></br>
+        <h3>Email Address:</h3>
+        <input type="text" id="lname" name="lname"></input>
+      </form>
+      <h3>Message:</h3>
+      <textarea id="email-body" rows="5" cols="100"></textarea>
+      <button onClick={sendMail}>Submit</button>
     </div>
   );
 }

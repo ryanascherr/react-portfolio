1a6ea373dd3593aad0a94a911a63ca1662b8d184
ryanascherr <ryanascherr@gmail.com>
2021-07-19 10:21:21 -0400

    Updated email section and added code to send an alert if all fields weren't filled in.

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,27 +1,29 @@
 import React from 'react';
+import $ from 'jquery';
 
 export default function Contact() {
 
   function sendMail() {
-    
-    
+    if ($("#name").val() == "" || $("#email").val() == "" || $("#email-body").val() == "") {
+      return alert('Please fill in all fields')
+    };
     var link = "mailto:ryanascherr@gmail.com"
-             + "?cc=myCCaddress@example.com"
-             + "&subject=" + encodeURIComponent("Hey Ryan, you're super cool!")
-             + "&body=" + encodeURIComponent(document.getElementById('email-body').value)
-    ;
-    
+      + "?cc=ryanascherr@gmail.com"
+      + "&subject=" + encodeURIComponent("Hey Ryan, you're super cool!")
+      + "&body=" + encodeURIComponent(document.getElementById('email-body').value)
+      ;
+
     window.location.href = link;
-}
+  }
 
   return (
     <div className="contact">
       <h1>Get in Touch</h1>
       <form>
         <h3>Name:</h3>
-        <input type="text" id="fname" name="fname"></input><br></br>
+        <input type="text" id="name" name="fname"></input><br></br>
         <h3>Email Address:</h3>
-        <input type="text" id="lname" name="lname"></input>
+        <input type="text" id="email" name="lname"></input>
       </form>
       <h3>Message:</h3>
       <textarea id="email-body" rows="5" cols="100"></textarea>
